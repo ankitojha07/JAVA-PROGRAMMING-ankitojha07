28d58ce56c22aae19f1be128a505e9de85e172d6
Ankit Ojha <ankitojha1409@gmail.com>
2022-06-17 16:12:33 +0530

    Input no. change

--- src/Algorithms/BinarySearch.java
+++ src/Algorithms/BinarySearch.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class BinarySearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10];
-        System.out.print("Enter 10 elements : ");
-        for (int i =0; i<10; i++){
+        int[] arr = new int[5];
+        System.out.print("Enter 5 elements : ");
+        for (int i =0; i<5; i++){
             arr[i] = sc.nextInt();
         }
         System.out.print("Enter value to search : ");
