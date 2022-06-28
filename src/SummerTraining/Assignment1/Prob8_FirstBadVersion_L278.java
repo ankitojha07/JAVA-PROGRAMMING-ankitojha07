daece525be4e3cf123653989c44edd73d7f9910b
ankittojha <ankitojha1409@gmail.com>
2022-06-28 09:20:17 +0530

    Assignment Prob-8 added
    First bad version

--- src/SummerTraining/Assignment1/Prob8_FirstBadVersion_L278.java
+++ src/SummerTraining/Assignment1/Prob8_FirstBadVersion_L278.java
@@ -5,3 +5,25 @@ public class Prob8_FirstBadVersion_L278 {
 
     }
 }
+// ANSWER BELOW : VersionControl clas is not present localhost that's why I have commented the solution
+
+//public class Solution extends VersionControl {
+//    public int firstBadVersion(int n) {
+//        int left = 0;
+//        int right = n;
+//
+//        while(left < right){
+//            int mid = left+(right-left)/2;
+//            if(!isBadVersion(mid)){
+//                left=mid+1;
+//            }
+//            else{
+//                if(isBadVersion(mid-1)){
+//                    right = mid-1;
+//                }
+//                else return mid;
+//            }
+//        }
+//        return -1;
+//    }
+//}
