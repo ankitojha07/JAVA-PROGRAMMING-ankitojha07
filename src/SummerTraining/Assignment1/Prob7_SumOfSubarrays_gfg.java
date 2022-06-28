f9f3081777d20a3ed1c37837025f770723c166ad
ankittojha <ankitojha1409@gmail.com>
2022-06-28 09:07:01 +0530

    Assignment Prob-7 added
    Sum of sub Arrays

--- src/SummerTraining/Assignment1/Prob7_SumOfSubarrays_gfg.java
+++ src/SummerTraining/Assignment1/Prob7_SumOfSubarrays_gfg.java
@@ -1,8 +1,24 @@
 package SummerTraining.Assignment1;
 
 public class Prob7_SumOfSubarrays_gfg {
+    public static long SubArraySum(int arr[], int n)
+    {
+        long result = 0,temp=0;
+        for (int i = 0; i < n; i ++)
+        {
+            temp=0;
+            for (int j = i; j < n; j ++)
+            {
+                temp+=arr[j];
+                result += temp ;
+            }
+        }
+        return result ;
+    }
     public static void main(String[] args) {
 
-
+        int arr[] = {1, 2, 3} ;
+        int n = arr.length;
+        System.out.println("Sum of SubArray : " + SubArraySum(arr, n));
     }
 }
