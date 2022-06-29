8a93a2126b0a76ad94d85104549d4db5c0fa5743
ankittojha <ankitojha1409@gmail.com>
2022-06-29 23:18:13 +0530

    Queue Implementation

--- src/DataStructures/QueueDS/QueueBasics.java
+++ src/DataStructures/QueueDS/QueueBasics.java
@@ -1,5 +1,41 @@
 package DataStructures.QueueDS;
 
-public class QueueBasics {
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data = data;
+    }
+}
+class QueueImplement{
+    Node front, rear;
+    public void enqueue(int data){
+        Node newNode = new Node(data);
+        if (front==null){
+            front = rear = newNode;
+            return;
+        }
+        rear.next = newNode;
+        rear = newNode;
+    }
 
+    public int dequeue() throws Exception {
+        if (front==null){
+            throw new Exception();
+        }
+        int result = front.data;
+        front = front.next;
+        return result;
+    }
+}
+public class QueueBasics {
+    public static void main(String[] args) throws Exception {
+        QueueImplement qi = new QueueImplement();
+        qi.enqueue(5);
+        qi.enqueue(9);
+        qi.enqueue(12);
+        qi.enqueue(66);
+
+        System.out.println(qi.dequeue());
+    }
 }
