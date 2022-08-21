a3dda123ecbd4e66056bf93cdd33e6291649dec3
Ankit Ojha <ankitojha1409@gmail.com>
2022-08-21 11:43:14 +0530

    Restarted DSA journey

---
+++ src/DataStructures/ArrayDs/TwoDArray/Basic2D.java
@@ -0,0 +1,13 @@
+package DataStructures.ArrayDs.TwoDArray;
+import java.util.Arrays;
+public class Basic2D {
+    public static void main(String[] args) {
+        int[][] nameArray;
+        nameArray = new int[2][2];
+        nameArray[0][0] = 1;
+        nameArray[0][1] = 2;
+        nameArray[1][0] = 3;
+        nameArray[1][1] = 4;
+        System.out.println(Arrays.deepToString(nameArray));
+    }
+}
