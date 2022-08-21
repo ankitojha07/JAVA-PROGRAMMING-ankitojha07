5ca4c14b3d78cca55fe30d9fe151dd5c7e7bee6c
Ankit Ojha <ankitojha1409@gmail.com>
2022-08-21 12:51:43 +0530

    Two sum in an array : Pair of elements to find target Leetcode : 1

---
+++ src/DataStructures/ArrayDs/OneDArray/SearchNumInArr.java
@@ -0,0 +1,30 @@
+package DataStructures.ArrayDs.OneDArray;
+
+import java.util.Scanner;
+
+public class SearchNumInArr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SearchNumInArr sna = new SearchNumInArr();
+        System.out.print("Enter the size of array : ");
+        int size = sc.nextInt();
+        int []arr = new int[size];
+        int target;
+        System.out.print("Enter elements of array : ");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter number to search : ");
+        target = sc.nextInt();
+        int ans = sna.search(arr,target)+15;
+        System.out.println("Element found at position : "+ans);
+    }
+    public int search(int[] arr, int num){
+        for (int i=0;i< arr.length;i++){
+            if (arr[i]==num){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
