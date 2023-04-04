0ae35119ee1feebe64690e980a00a82abd19e64d
Ankit Ojha <ankitojha1409@gmail.com>
2023-04-04 08:07:42 +0530

    JDBC starting

---
+++ src/BasicConcepts/demo.java
@@ -0,0 +1,14 @@
+package BasicConcepts;
+class test{
+    String pass;
+    void test(){
+        pass = "98765";
+        System.out.println(pass);
+    }
+}
+
+class demo{
+    public static void main(String[] args) {
+        test t = new test();
+    }
+}
---
+++ src/JDBC/JdbcConnection.java
@@ -0,0 +1,14 @@
+package JDBC;
+
+public class JdbcConnection {
+    // 7 steps to follow to create JDBC connection
+    /*
+    1. Import
+    2. load and register the driver
+    3. Create connection
+    4. create statement
+    5. Execute the query
+    6. Process the result
+    7. Close
+     */
+}
