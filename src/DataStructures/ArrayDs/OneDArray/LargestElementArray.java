a85164b7b7af3bc07e35792a2dcccd0e2b9e30b7
Ankit <ankitojha1409@gmail.com>
2024-03-16 01:42:39 +0530

    Largest element of the array solved

--- src/DataStructures/ArrayDs/OneDArray/LargestElementArray.java
+++ src/DataStructures/ArrayDs/OneDArray/LargestElementArray.java
@@ -1,5 +1,8 @@
 package DataStructures.ArrayDs.OneDArray;
 
+import java.sql.Array;
+import java.util.Scanner;
+
 public class LargestElementArray {
     static int largestElem(int[] arr){
         int temp=0;
@@ -9,5 +12,16 @@ public class LargestElementArray {
             }
         }
         return temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr1 = new int[6];
+        System.out.println("Enter the next "+ (arr1.length-1) +" elements of the array : ");
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = sc.nextInt();
+        }
+        int largeElement = largestElem(arr1);
+        System.out.println("Largest element of the array is : "+largeElement);
     }
 }
